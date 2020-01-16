a34567f6e6d3e288a8348fcb3b9320c8b6d0a338
Gregor Zadnik <gregor.zadnik@gmail.com>
2020-01-16 11:20:44 +0000

    Verjetno koncan REST

--- api/env/api.py
+++ api/env/api.py
@@ -21,11 +21,26 @@ knjige = [
      'izdana': 1975}
 ]
 
-#nova = '{"id" : 3, "naslov" : "The Silmarillion", "avtor" : "J. R. R. Tolkien", "izdaja" : 1977}'
+izposoje = [
+    {
+        'idKnjige' : 0,
+        'ime' : 'Gregor',
+        'priimek' : 'Zadnik'
+    },
+    {
+        'idKnjige' : 2,
+        'ime' : 'Jure',
+        'priimek' : 'Jesenšek'
+    }
+    ]
+
+#novaKnjiga = '{"id" : 3, "naslov" : "The Silmarillion", "avtor" : "J. R. R. Tolkien", "izdaja" : 1977}'
+#novaIzposoja = 
 @app.route('/', methods=['GET'])
 def home():
     return 'Imamo knjige za sposodit!'
 
+# Najde knjigo z idjem id ter jo vrne
 @app.route("/knjiga/<id>", methods=['GET'])
 def vrni_knjigo(id):
     result = []
@@ -35,13 +50,27 @@ def vrni_knjigo(id):
             result.append(knjiga)
     return jsonify(result)
 
-# A route to return all of the available entries in our catalog.
-@app.route('/knjige', methods=['GET'])
-def vrni_vse():
-    return jsonify(knjige)
-    
+# Najde izposoje za iporabnika z imenom ime    
+@app.route("/izposoje/<ime>", methods=['GET'])
+def vrni_izposoje_uporabnika(ime):
+    result = []
+    for izposoja in izposoje:
+        if izposoja['ime'] == ime:
+            result.append(izposoja)
+    return jsonify(result)
 
 # A route to return all of the available entries in our catalog.
+@app.route('/knjige', methods=['GET'])
+def vrni_knjige():
+    return jsonify(knjige)
+    
+# Prikaze vse izposoje
+@app.route('/izposoje', methods=['GET'])
+def vrni_izposoje():
+    return jsonify(izposoje)
+    
+
+# Doda novo knjigo
 @app.route('/novaKnjiga', methods=['POST'])
 def dodaj_knjigo():
     id = request.json['id']
@@ -51,6 +80,29 @@ def dodaj_knjigo():
     novaKnjiga = {'id' : id, 'naslov' : naslov, 'avtor' : avtor, 'izdana' : izdana}
     knjige.append(novaKnjiga)
     return jsonify(novaKnjiga)
+
+# Naredi izposojo za podatke, ki jih je uporabnik vnesel, ce knjiga se ni izposojena
+@app.route('/izposodiKnjigo', methods=['POST'])
+def izposodi_knjigo():
+    idKnjige = request.json['idKnjige']
+    ime = request.json['ime']
+    priimek = request.json['priimek']
+    for izposoja in izposoje:
+        if izposoja['idKnjige'] == idKnjige:
+            return "Knjiga je ze izposojena!"
+    novaIzposoja = {'idKnjige' : idKnjige, 'ime' : ime, 'priimek' : priimek}
+    izposoje.append(novaIzposoja)
+    return jsonify(novaIzposoja)
+    
+# Vrne knjigo z IDjem id
+@app.route('/vrniKnjigo', methods=['POST'])
+def vrni_izposojeno_knjigo():
+    idKnjige = request.json['idKnjige']
+    for izposoja in izposoje:
+        if izposoja['idKnjige'] == idKnjige:
+            izposoje.remove(izposoja)
+            return "Knjiga uspesno vrnjena."
+    return "Knjiga se ni izposojena."
     
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
